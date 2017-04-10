cd76b2e0b82f84f29ebdf1d3d3f461df33619272
no <no>
2017-04-10 09:10:44 +0300

    align

--- src/containers/Editor.js
+++ src/containers/Editor.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { actions as editorActions } from '../redux/modules/Editor';
+import { actions as editorActions, constants as editorConstants } from '../redux/modules/Editor';
 import { stringToLP } from '../utils/linja-pona-parser';
 import { Well, Col, FormGroup, ControlLabel, FormControl, Button, Glyphicon } from 'react-bootstrap';
 import Navbar from './Navbar';
+import cx from 'classnames';
 
 const propTypes = {};
 
@@ -15,30 +16,49 @@ class Editor extends Component {
     }
 
     render() {
-        const { props: { editorInput } } = this;
+        const { props: { editorInput, editorAlign, editorAlignLeft, editorAlignCenter }, handleChange } = this;
+        const isAlignLeft = editorAlign === editorConstants.ALIGN_LEFT;
+        const isAlignCenter = editorAlign === editorConstants.ALIGN_CENTER;
 
         return (
             <div>
                 <Navbar />
                 <div className="container-responsive">
-
                     <Col xs={12} sm={6} className="pull-right">
                         <ControlLabel>o lukin:</ControlLabel>
-                        <Well bsSize="small" className="font-linja-pona font-size-big">{stringToLP(editorInput)}</Well>
+                        <Well
+                            bsSize="small"
+                            className={
+                                cx('font-linja-pona font-size-big', {
+                                    alignLeft: isAlignLeft,
+                                    alignCenter: isAlignCenter
+                                })
+                            }
+                        >
+                            {stringToLP(editorInput)}
+                        </Well>
                         <FormGroup>
-                            <Button bsStyle="primary"><Glyphicon glyph="align-left" /></Button>
+                            <Button bsStyle="primary" onClick={editorAlignLeft} className={cx({ active: isAlignLeft })}>
+                                <Glyphicon glyph="align-left" />
+                            </Button>
                             {' '}
-                            <Button bsStyle="primary"><Glyphicon glyph="align-center" /></Button>
+                            <Button
+                                bsStyle="primary"
+                                onClick={editorAlignCenter}
+                                className={cx({ active: isAlignCenter })}
+                            >
+                                <Glyphicon glyph="align-center" />
+                            </Button>
+                            {' '}
                             <Button bsStyle="primary" className="pull-right">kama jo e sitelen</Button>
                         </FormGroup>
-
                     </Col>
                     <Col xs={12} sm={6}>
                         <FormGroup>
                             <ControlLabel>o sitelen:</ControlLabel>
                             <FormControl
                                 componentClass="textarea"
-                                onChange={this.handleChange.bind(this)}
+                                onChange={handleChange.bind(this)}
                                 value={editorInput}
                             />
                         </FormGroup>
@@ -50,14 +70,14 @@ class Editor extends Component {
 }
 
 const mapStateToProps = state => {
-    return {
-        editorInput: state.editor.input
-    };
+    return { editorInput: state.editor.input, editorAlign: state.editor.align };
 };
 const mapDispatchToProps = dispatch => {
     return bindActionCreators(
         {
-            editorChange: editorActions.editorChange
+            editorChange: editorActions.editorChange,
+            editorAlignLeft: editorActions.editorAlignLeft,
+            editorAlignCenter: editorActions.editorAlignCenter
         },
         dispatch
     );
--- src/redux/modules/Editor.js
+++ src/redux/modules/Editor.js
@@ -1,20 +1,29 @@
 // Constants
 
 export const constants = {
-    EDITOR_CHANGE: 'EDITOR_CHANGE'
+    EDITOR_CHANGE: 'EDITOR_CHANGE',
+    EDITOR_ALIGN_CHANGE: 'EDITOR_ALIGN_CHANGE',
+    ALIGN_LEFT: 'LEFT',
+    ALIGN_CENTER: 'CENTER'
 };
 
 // Action Creators
 
-export const actions = { editorChange: payload => ({ type: constants.EDITOR_CHANGE, payload }) };
+export const actions = {
+    editorChange: payload => ({ type: constants.EDITOR_CHANGE, payload }),
+    editorAlignLeft: _ => ({ type: constants.EDITOR_ALIGN_CHANGE, payload:constants.ALIGN_LEFT }),
+    editorAlignCenter: _ => ({ type: constants.EDITOR_ALIGN_CHANGE, payload:constants.ALIGN_CENTER })
+};
 
 // Reducer
-export const defaultState = { input: '' };
+export const defaultState = { input: '', align: constants.ALIGN_LEFT };
 
 export default function(state = defaultState, action) {
     switch (action.type) {
         case constants.EDITOR_CHANGE:
             return { ...state, input: action.payload };
+        case constants.EDITOR_ALIGN_CHANGE:
+            return { ...state, align: action.payload };
         default:
             return state;
     }
